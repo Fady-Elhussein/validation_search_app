ea0c15a54b36e9d790e64ae4ef26f5d0b06e2149
Fady Elhussein <83413691+Fady-Elhussein@users.noreply.github.com>
2024-11-10 13:33:37 +0200

    Update search_page.dart

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -1,46 +1,104 @@
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatelessWidget{
-   SearchPage({super.key});
-  TextEditingController searchController=TextEditingController();
-   final formKey = GlobalKey<FormState>();
+  SearchPage({super.key});
+TextEditingController searchController=TextEditingController();
+  TextEditingController searchController2=TextEditingController();
 
+final formKey=GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(),
-      body:Form(
-        key: formKey,
-        child: Column(
-          children: [
-            TextFormField(
-              keyboardType: TextInputType.text,
-              controller: searchController,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Field is Required ';
-                }
-                return null;
-              },
-              decoration: InputDecoration(
-                labelText: "Search ",
-                border: OutlineInputBorder(),
-                suffixIcon: Icon(Icons.search),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller:searchController ,
+                validator: (searchText) {
+                  if(searchText==null || searchText.isEmpty )
+                    {
+                      return "Field is Required ";
+
+                    }
+
+
+                  return null;
+
+                },
+                decoration: InputDecoration(
+                  hintText: "Search",
+                  label: Text("Search"),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  )
+                ),
               ),
-            ),
-            Container(
-              width: double.infinity,
-              child: MaterialButton(onPressed: () {
-                if (formKey.currentState!.validate()) {
+              TextFormField(
+                controller:searchController2 ,
+                validator: (searchText) {
+                  if(searchText==null || searchText.isEmpty )
+                  {
+                    return "Field2 is Required ";
+
+                  }
+                  if(searchText.length>8){
+                    return "more letter Required ";
+
+                  }
 
 
-                }
-              },child: Text("Search"),),
-            )
-          ],
+                  return null;
+
+                },
+                decoration: InputDecoration(
+                    hintText: "Search",
+                    label: Text("Search"),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    )
+                ),
+              ),
+              TextFormField(
+                controller:searchController ,
+                validator: (searchText) {
+                  if(searchText==null || searchText.isEmpty )
+                  {
+                    return "Field is Required ";
+
+                  }
+
+
+                  return null;
+
+                },
+                decoration: InputDecoration(
+                    hintText: "Search",
+                    label: Text("Search"),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    )
+                ),
+              ),
+              TextButton(onPressed: (){
+                if(formKey.currentState!.validate()){
+
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text(searchController.text)));
+
+                    }
+                  }, child: Text("Search")),
+            ],
+          ),
         ),
       ),
     );
   }
 
 }
+
+
+
